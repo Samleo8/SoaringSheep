0502672cf83edad564db6a59fc08ca405a61ad9f
Samuel Leong <leowhiz@yahoo.com.sg>
2019-07-06 23:00:51 +0800

    Added telegram specific stuff

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 
 var requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame;
 
-var forceIsApp = true;
+var forceIsApp = false;
 
 var _isApp = null;
 var _isAndroid = null;
@@ -12,6 +12,33 @@ var _isMobile = false;
 
 var Game;
 var GPlay;
+
+function getIsTelegram() {
+  var qs = getQueryString();
+  return !!qs.inlineId || !!qs.chatId || !!qs.messageId || !!qs.userId;
+}
+
+function getQueryString() {
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+      // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+      query_string[pair[0]] = arr;
+      // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
+}
+
 
 var app = {
     // Application Constructor
