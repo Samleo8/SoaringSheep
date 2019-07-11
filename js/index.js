7f53740aeabc06f550aecbe6555b3bb3678ab2fb
Samuel Leong Chee Weng <Samleo8@users.noreply.github.com>
2019-07-11 20:48:11 +0800

    Added alert debugging

--- js/index.js
+++ js/index.js
@@ -4594,7 +4594,7 @@ var SoaringSheepGame = function(){
 			var xmlhttp = new XMLHttpRequest();
 
 			var url = "https://samstudiosbot.now.sh/highscore/SoaringSheep/"+ this.score + "?id="+playerid;
-alert(playerid + " " + url);
+                        alert(playerid + " " + url);
 
 			xmlhttp.open("GET", url, true);
 
