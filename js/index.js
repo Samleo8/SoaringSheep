b334b8c98b4b12887c425b83bbc232640e33336c
Samuel Leong <leowhiz@yahoo.com.sg>
2019-07-11 22:43:01 +0800

    Debug 3

--- js/index.js
+++ js/index.js
@@ -14,33 +14,9 @@ var Game;
 var GPlay;
 
 function getIsTelegram() {
-  return window.location.href.indexOf("?id=")!=-1;
-
-  var qs = getQueryString();
-  return !!qs.inlineId || !!qs.chatId || !!qs.messageId || !!qs.userId;
+	//return !!window.TelegramGameProxy;
+	return window.location.href.indexOf("id=")!=-1;
 }
-
-function getQueryString() {
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-      // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
-      query_string[pair[0]] = arr;
-      // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  }
-  return query_string;
-}
-
 
 var app = {
     // Application Constructor
