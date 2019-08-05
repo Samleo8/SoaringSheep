7ba51eaa23dae9ae9b1f6f7a8b329344a65715fd
Samuel Leong <leowhiz@yahoo.com.sg>
2019-08-05 15:06:07 +0800

    Using accessories names to avoid app throwing issue where hero sheep is undefined

--- js/index.js
+++ js/index.js
@@ -410,6 +410,16 @@ var SoaringSheepGame = function(){
 
 		this.accessoriesNames.push(ii.toString());
 
+		//To avoid issues where sheep base is not initialised first, place skins at the front, so that they will be handled first
+		//Inefficient, but it works
+		if(this.accessories[ii].type == "skin"){
+			this.accessoriesNames.unshift(ii.toString());
+		}
+		else{
+			this.accessoriesNames.push(ii.toString());
+		}
+
+
 		if(ii == "no_cape" || ii=="no_hat") continue;
 
 		if(this.accessories[ii].type == "hat") this.hatNames.push(ii.toString());
@@ -3853,12 +3863,13 @@ var SoaringSheepGame = function(){
 			this.accessories = JSON.parse(localStorage["accessories"]);
 		}
 
-		var i;
-		for(i in this.accessories){
-			if(!this.accessories.hasOwnProperty(i)) continue;
+		var i, nm;
+		for(i=0;i<this.accessoriesNames.length;i++){
+			//if(!this.accessories.hasOwnProperty(i)) continue;
+			nm = this.accessoriesNames[i];
 
-			if(this.accessories[i].activated){
-				this.setAccessory(i.toString(), this.accessories[i].type);
+			if(this.accessories[nm].activated){
+				this.setAccessory(nm, this.accessories[nm].type);
 			}
 		}
 
